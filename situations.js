8901cd8c111498b4218d365efa44345225629a98
Noah Math <noah4@berkeley.edu>
2025-02-23 17:19:53 -0800

    removed redundant randomness

--- situations.js
+++
@@ -1,7 +0,0 @@
-//start is always right, gap always left
-//store L-spawning, path
-
-const situations = [[true, "toBottomLeft"], [true, "throughGapStop"],
-                    [true, "throughGapReturn"], [true, "toTopRight"],
-                    [false, "toBottomLeft"], [false, "throughGapStop"]
-                    [false, "throughGapReturn"]];
